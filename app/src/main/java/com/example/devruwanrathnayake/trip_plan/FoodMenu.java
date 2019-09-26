017104841d53a58c3c86cd1f347fbac0d1cc72af
Sewvandi98 <sewvandi.promodya@gmail.com>
2019-09-26 07:23:25 +0530

    Mad Project

---
+++ app/src/main/java/com/example/devruwanrathnayake/trip_plan/FoodMenu.java
@@ -0,0 +1,13 @@
+package com.example.devruwanrathnayake.trip_plan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_menu);
+    }
+}
